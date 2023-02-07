a8178b7543b5fc0c145ca0d40d92a165cf24d791
frankrank <72446@192.168.199.1>
2023-02-07 14:23:32 +0800

    增加了一个唱片

---
+++ src/main/java/net/mcreator/difficultmod/item/OnlyforikunItem.java
@@ -0,0 +1,45 @@
+
+package net.mcreator.difficultmod.item;
+
+import net.minecraftforge.registries.ObjectHolder;
+import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.api.distmarker.OnlyIn;
+import net.minecraftforge.api.distmarker.Dist;
+
+import net.minecraft.util.ResourceLocation;
+import net.minecraft.item.Rarity;
+import net.minecraft.item.MusicDiscItem;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.item.Item;
+
+import net.mcreator.difficultmod.SuperdifficultmodModElements;
+
+@SuperdifficultmodModElements.ModElement.Tag
+public class OnlyforikunItem extends SuperdifficultmodModElements.ModElement {
+	@ObjectHolder("superdifficultmod:onlyforikun")
+	public static final Item block = null;
+
+	public OnlyforikunItem(SuperdifficultmodModElements instance) {
+		super(instance, 22);
+	}
+
+	@Override
+	public void initElements() {
+		elements.items.add(() -> new MusicDiscItemCustom());
+	}
+
+	public static class MusicDiscItemCustom extends MusicDiscItem {
+		public MusicDiscItemCustom() {
+			super(0, (net.minecraft.util.SoundEvent) ForgeRegistries.SOUND_EVENTS.getValue(new ResourceLocation("entity.chicken.step")),
+					new Item.Properties().group(ItemGroup.MISC).maxStackSize(1).rarity(Rarity.RARE));
+			setRegistryName("onlyforikun");
+		}
+
+		@Override
+		@OnlyIn(Dist.CLIENT)
+		public boolean hasEffect(ItemStack itemstack) {
+			return true;
+		}
+	}
+}
