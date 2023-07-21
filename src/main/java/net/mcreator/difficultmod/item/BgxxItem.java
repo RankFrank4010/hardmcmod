8a8d9c1c190bc815de3d95e21671af7bf1c76eae
frankrank <30345@DESKTOP-VP7JU1O>
2023-07-21 16:55:30 +0800

    这是什么鬼！

---
+++ src/main/java/net/mcreator/difficultmod/item/BgxxItem.java
@@ -0,0 +1,67 @@
+
+package net.mcreator.difficultmod.item;
+
+import net.minecraftforge.registries.ObjectHolder;
+
+import net.minecraft.world.World;
+import net.minecraft.util.text.StringTextComponent;
+import net.minecraft.util.text.ITextComponent;
+import net.minecraft.item.UseAction;
+import net.minecraft.item.Rarity;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.item.Item;
+import net.minecraft.client.util.ITooltipFlag;
+import net.minecraft.block.BlockState;
+
+import net.mcreator.difficultmod.SuperdifficultmodModElements;
+
+import java.util.List;
+
+@SuperdifficultmodModElements.ModElement.Tag
+public class BgxxItem extends SuperdifficultmodModElements.ModElement {
+	@ObjectHolder("superdifficultmod:bgxx")
+	public static final Item block = null;
+
+	public BgxxItem(SuperdifficultmodModElements instance) {
+		super(instance, 30);
+	}
+
+	@Override
+	public void initElements() {
+		elements.items.add(() -> new ItemCustom());
+	}
+
+	public static class ItemCustom extends Item {
+		public ItemCustom() {
+			super(new Item.Properties().group(ItemGroup.TOOLS).maxStackSize(1).isImmuneToFire().rarity(Rarity.UNCOMMON));
+			setRegistryName("bgxx");
+		}
+
+		@Override
+		public UseAction getUseAction(ItemStack itemstack) {
+			return UseAction.EAT;
+		}
+
+		@Override
+		public int getItemEnchantability() {
+			return 0;
+		}
+
+		@Override
+		public float getDestroySpeed(ItemStack par1ItemStack, BlockState par2Block) {
+			return 8F;
+		}
+
+		@Override
+		public boolean canHarvestBlock(BlockState state) {
+			return true;
+		}
+
+		@Override
+		public void addInformation(ItemStack itemstack, World world, List<ITextComponent> list, ITooltipFlag flag) {
+			super.addInformation(itemstack, world, list, flag);
+			list.add(new StringTextComponent("YzhBgxX!"));
+		}
+	}
+}
