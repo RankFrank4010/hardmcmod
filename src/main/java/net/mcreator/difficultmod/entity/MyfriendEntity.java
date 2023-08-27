4e8b982cad2e593adcda9d2a1ed4045a85434aef
frankrank <30345@DESKTOP-VP7JU1O>
2023-08-27 10:59:48 +0800

    增加了我的伙伴

---
+++ src/main/java/net/mcreator/difficultmod/entity/MyfriendEntity.java
@@ -0,0 +1,311 @@
+
+package net.mcreator.difficultmod.entity;
+
+import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.items.wrapper.EntityHandsInvWrapper;
+import net.minecraftforge.items.wrapper.EntityArmorInvWrapper;
+import net.minecraftforge.items.wrapper.CombinedInvWrapper;
+import net.minecraftforge.items.ItemStackHandler;
+import net.minecraftforge.items.CapabilityItemHandler;
+import net.minecraftforge.fml.network.NetworkHooks;
+import net.minecraftforge.fml.network.FMLPlayMessages;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.event.entity.EntityAttributeCreationEvent;
+import net.minecraftforge.common.util.LazyOptional;
+import net.minecraftforge.common.capabilities.Capability;
+
+import net.minecraft.world.World;
+import net.minecraft.util.text.StringTextComponent;
+import net.minecraft.util.text.ITextComponent;
+import net.minecraft.util.math.vector.Vector3d;
+import net.minecraft.util.math.MathHelper;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.ResourceLocation;
+import net.minecraft.util.Hand;
+import net.minecraft.util.Direction;
+import net.minecraft.util.DamageSource;
+import net.minecraft.util.ActionResultType;
+import net.minecraft.network.PacketBuffer;
+import net.minecraft.network.IPacket;
+import net.minecraft.nbt.INBT;
+import net.minecraft.nbt.CompoundNBT;
+import net.minecraft.item.crafting.Ingredient;
+import net.minecraft.item.SpawnEggItem;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.item.Item;
+import net.minecraft.inventory.container.INamedContainerProvider;
+import net.minecraft.inventory.container.Container;
+import net.minecraft.entity.projectile.PotionEntity;
+import net.minecraft.entity.projectile.AbstractArrowEntity;
+import net.minecraft.entity.player.ServerPlayerEntity;
+import net.minecraft.entity.player.PlayerInventory;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.entity.ai.goal.TemptGoal;
+import net.minecraft.entity.ai.goal.SwimGoal;
+import net.minecraft.entity.ai.goal.RandomWalkingGoal;
+import net.minecraft.entity.ai.goal.MeleeAttackGoal;
+import net.minecraft.entity.ai.goal.LookRandomlyGoal;
+import net.minecraft.entity.ai.goal.HurtByTargetGoal;
+import net.minecraft.entity.ai.attributes.Attributes;
+import net.minecraft.entity.ai.attributes.AttributeModifierMap;
+import net.minecraft.entity.MobEntity;
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.EntityType;
+import net.minecraft.entity.EntityClassification;
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.CreatureEntity;
+import net.minecraft.entity.CreatureAttribute;
+import net.minecraft.entity.AreaEffectCloudEntity;
+import net.minecraft.enchantment.EnchantmentHelper;
+
+import net.mcreator.difficultmod.item.TianYuItem;
+import net.mcreator.difficultmod.gui.MyhuozhuiGui;
+import net.mcreator.difficultmod.entity.renderer.MyfriendRenderer;
+import net.mcreator.difficultmod.block.BgxxblockBlock;
+import net.mcreator.difficultmod.SuperdifficultmodModElements;
+
+import javax.annotation.Nullable;
+import javax.annotation.Nonnull;
+
+import io.netty.buffer.Unpooled;
+
+@SuperdifficultmodModElements.ModElement.Tag
+public class MyfriendEntity extends SuperdifficultmodModElements.ModElement {
+	public static EntityType entity = (EntityType.Builder.<CustomEntity>create(CustomEntity::new, EntityClassification.MISC)
+			.setShouldReceiveVelocityUpdates(true).setTrackingRange(1024).setUpdateInterval(3).setCustomClientFactory(CustomEntity::new)
+			.immuneToFire().size(0.9f, 0.9f)).build("myfriend").setRegistryName("myfriend");
+
+	public MyfriendEntity(SuperdifficultmodModElements instance) {
+		super(instance, 37);
+		FMLJavaModLoadingContext.get().getModEventBus().register(new MyfriendRenderer.ModelRegisterHandler());
+		FMLJavaModLoadingContext.get().getModEventBus().register(new EntityAttributesRegisterHandler());
+	}
+
+	@Override
+	public void initElements() {
+		elements.entities.add(() -> entity);
+		elements.items.add(
+				() -> new SpawnEggItem(entity, -39373, -39322, new Item.Properties().group(ItemGroup.MISC)).setRegistryName("myfriend_spawn_egg"));
+	}
+
+	@Override
+	public void init(FMLCommonSetupEvent event) {
+	}
+
+	private static class EntityAttributesRegisterHandler {
+		@SubscribeEvent
+		public void onEntityAttributeCreation(EntityAttributeCreationEvent event) {
+			AttributeModifierMap.MutableAttribute ammma = MobEntity.func_233666_p_();
+			ammma = ammma.createMutableAttribute(Attributes.MOVEMENT_SPEED, 8);
+			ammma = ammma.createMutableAttribute(Attributes.MAX_HEALTH, 512);
+			ammma = ammma.createMutableAttribute(Attributes.ARMOR, 48);
+			ammma = ammma.createMutableAttribute(Attributes.ATTACK_DAMAGE, 13);
+			ammma = ammma.createMutableAttribute(Attributes.FOLLOW_RANGE, 16);
+			ammma = ammma.createMutableAttribute(Attributes.KNOCKBACK_RESISTANCE, 5);
+			ammma = ammma.createMutableAttribute(Attributes.ATTACK_KNOCKBACK, 2);
+			event.put(entity, ammma.create());
+		}
+	}
+
+	public static class CustomEntity extends CreatureEntity {
+		public CustomEntity(FMLPlayMessages.SpawnEntity packet, World world) {
+			this(entity, world);
+		}
+
+		public CustomEntity(EntityType<CustomEntity> type, World world) {
+			super(type, world);
+			experienceValue = 256;
+			setNoAI(false);
+			setCustomName(new StringTextComponent("你的伙伴"));
+			setCustomNameVisible(true);
+			enablePersistence();
+		}
+
+		@Override
+		public IPacket<?> createSpawnPacket() {
+			return NetworkHooks.getEntitySpawningPacket(this);
+		}
+
+		@Override
+		protected void registerGoals() {
+			super.registerGoals();
+			this.goalSelector.addGoal(1, new MeleeAttackGoal(this, 5, false) {
+				@Override
+				protected double getAttackReachSqr(LivingEntity entity) {
+					return (double) (4.0 + entity.getWidth() * entity.getWidth());
+				}
+			});
+			this.goalSelector.addGoal(2, new RandomWalkingGoal(this, 1.2));
+			this.targetSelector.addGoal(3, new HurtByTargetGoal(this));
+			this.goalSelector.addGoal(4, new LookRandomlyGoal(this));
+			this.goalSelector.addGoal(5, new SwimGoal(this));
+			this.goalSelector.addGoal(6, new TemptGoal(this, 2, Ingredient.fromItems(TianYuItem.block), false));
+		}
+
+		@Override
+		public CreatureAttribute getCreatureAttribute() {
+			return CreatureAttribute.UNDEFINED;
+		}
+
+		@Override
+		public boolean canDespawn(double distanceToClosestPlayer) {
+			return false;
+		}
+
+		protected void dropSpecialItems(DamageSource source, int looting, boolean recentlyHitIn) {
+			super.dropSpecialItems(source, looting, recentlyHitIn);
+			this.entityDropItem(new ItemStack(BgxxblockBlock.block));
+		}
+
+		@Override
+		public net.minecraft.util.SoundEvent getHurtSound(DamageSource ds) {
+			return (net.minecraft.util.SoundEvent) ForgeRegistries.SOUND_EVENTS.getValue(new ResourceLocation("entity.fox.hurt"));
+		}
+
+		@Override
+		public net.minecraft.util.SoundEvent getDeathSound() {
+			return (net.minecraft.util.SoundEvent) ForgeRegistries.SOUND_EVENTS.getValue(new ResourceLocation("entity.fox.death"));
+		}
+
+		@Override
+		public boolean attackEntityFrom(DamageSource source, float amount) {
+			if (source.getImmediateSource() instanceof AbstractArrowEntity)
+				return false;
+			if (source.getImmediateSource() instanceof PlayerEntity)
+				return false;
+			if (source.getImmediateSource() instanceof PotionEntity || source.getImmediateSource() instanceof AreaEffectCloudEntity)
+				return false;
+			if (source == DamageSource.FALL)
+				return false;
+			if (source == DamageSource.CACTUS)
+				return false;
+			if (source == DamageSource.DROWN)
+				return false;
+			if (source == DamageSource.LIGHTNING_BOLT)
+				return false;
+			if (source.isExplosion())
+				return false;
+			if (source.getDamageType().equals("trident"))
+				return false;
+			if (source == DamageSource.ANVIL)
+				return false;
+			if (source == DamageSource.DRAGON_BREATH)
+				return false;
+			if (source == DamageSource.WITHER)
+				return false;
+			if (source.getDamageType().equals("witherSkull"))
+				return false;
+			return super.attackEntityFrom(source, amount);
+		}
+
+		private final ItemStackHandler inventory = new ItemStackHandler(32) {
+			@Override
+			public int getSlotLimit(int slot) {
+				return 64;
+			}
+		};
+		private final CombinedInvWrapper combined = new CombinedInvWrapper(inventory, new EntityHandsInvWrapper(this),
+				new EntityArmorInvWrapper(this));
+
+		@Override
+		public <T> LazyOptional<T> getCapability(@Nonnull Capability<T> capability, @Nullable Direction side) {
+			if (this.isAlive() && capability == CapabilityItemHandler.ITEM_HANDLER_CAPABILITY && side == null)
+				return LazyOptional.of(() -> combined).cast();
+			return super.getCapability(capability, side);
+		}
+
+		@Override
+		protected void dropInventory() {
+			super.dropInventory();
+			for (int i = 0; i < inventory.getSlots(); ++i) {
+				ItemStack itemstack = inventory.getStackInSlot(i);
+				if (!itemstack.isEmpty() && !EnchantmentHelper.hasVanishingCurse(itemstack)) {
+					this.entityDropItem(itemstack);
+				}
+			}
+		}
+
+		@Override
+		public void writeAdditional(CompoundNBT compound) {
+			super.writeAdditional(compound);
+			compound.put("InventoryCustom", inventory.serializeNBT());
+		}
+
+		@Override
+		public void readAdditional(CompoundNBT compound) {
+			super.readAdditional(compound);
+			INBT inventoryCustom = compound.get("InventoryCustom");
+			if (inventoryCustom instanceof CompoundNBT)
+				inventory.deserializeNBT((CompoundNBT) inventoryCustom);
+		}
+
+		@Override
+		public ActionResultType func_230254_b_(PlayerEntity sourceentity, Hand hand) {
+			ItemStack itemstack = sourceentity.getHeldItem(hand);
+			ActionResultType retval = ActionResultType.func_233537_a_(this.world.isRemote());
+			if (sourceentity.isSecondaryUseActive()) {
+				if (sourceentity instanceof ServerPlayerEntity) {
+					NetworkHooks.openGui((ServerPlayerEntity) sourceentity, new INamedContainerProvider() {
+						@Override
+						public ITextComponent getDisplayName() {
+							return new StringTextComponent("我的伙伴");
+						}
+
+						@Override
+						public Container createMenu(int id, PlayerInventory inventory, PlayerEntity player) {
+							PacketBuffer packetBuffer = new PacketBuffer(Unpooled.buffer());
+							packetBuffer.writeBlockPos(new BlockPos(sourceentity.getPosition()));
+							packetBuffer.writeByte(0);
+							packetBuffer.writeVarInt(CustomEntity.this.getEntityId());
+							return new MyhuozhuiGui.GuiContainerMod(id, inventory, packetBuffer);
+						}
+					}, buf -> {
+						buf.writeBlockPos(new BlockPos(sourceentity.getPosition()));
+						buf.writeByte(0);
+						buf.writeVarInt(this.getEntityId());
+					});
+				}
+				return ActionResultType.func_233537_a_(this.world.isRemote());
+			}
+			super.func_230254_b_(sourceentity, hand);
+			sourceentity.startRiding(this);
+			return retval;
+		}
+
+		@Override
+		public void travel(Vector3d dir) {
+			Entity entity = this.getPassengers().isEmpty() ? null : (Entity) this.getPassengers().get(0);
+			if (this.isBeingRidden()) {
+				this.rotationYaw = entity.rotationYaw;
+				this.prevRotationYaw = this.rotationYaw;
+				this.rotationPitch = entity.rotationPitch * 0.5F;
+				this.setRotation(this.rotationYaw, this.rotationPitch);
+				this.jumpMovementFactor = this.getAIMoveSpeed() * 0.15F;
+				this.renderYawOffset = entity.rotationYaw;
+				this.rotationYawHead = entity.rotationYaw;
+				this.stepHeight = 1.0F;
+				if (entity instanceof LivingEntity) {
+					this.setAIMoveSpeed((float) this.getAttributeValue(Attributes.MOVEMENT_SPEED));
+					float forward = ((LivingEntity) entity).moveForward;
+					float strafe = ((LivingEntity) entity).moveStrafing;
+					super.travel(new Vector3d(strafe, 0, forward));
+				}
+				this.prevLimbSwingAmount = this.limbSwingAmount;
+				double d1 = this.getPosX() - this.prevPosX;
+				double d0 = this.getPosZ() - this.prevPosZ;
+				float f1 = MathHelper.sqrt(d1 * d1 + d0 * d0) * 4.0F;
+				if (f1 > 1.0F)
+					f1 = 1.0F;
+				this.limbSwingAmount += (f1 - this.limbSwingAmount) * 0.4F;
+				this.limbSwing += this.limbSwingAmount;
+				return;
+			}
+			this.stepHeight = 0.5F;
+			this.jumpMovementFactor = 0.02F;
+			super.travel(dir);
+		}
+	}
+}
